4f671a19bf460f2647978bee4a6f29d0090b6f05
sachakr <sachakr@microsoft.com>
2023-08-04 13:29:53 +0530

    02-cognitive-services-security - Manage Cognitive Services Security

--- 02-cognitive-security/rest-test.cmd
+++ 02-cognitive-security/rest-test.cmd
@@ -1,1 +1,1 @@
-curl -X POST "<yourEndpoint>/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: <yourKey>" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
+curl -X POST "https://cs-sachakr-demo.cognitiveservices.azure.com/text/analytics/v3.1/languages?" -H "Content-Type: application/json" -H "Ocp-Apim-Subscription-Key: 56979e4de1df471d9541338c9a11c234" --data-ascii "{'documents':[{'id':1,'text':'hello'}]}"
